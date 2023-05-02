dd74fff6a7281fa1c8cc3f89e05bf0057c6cb088
raphaelpg <raphael_pinto@outlook.com>
2023-05-02 17:40:54 +0200

    add createMany txs

--- practical/src/modules/transactions/transactions.services.ts
+++ practical/src/modules/transactions/transactions.services.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Transaction } from './transactions.entity';
 
 @Injectable()
@@ -8,10 +8,29 @@ export class TransactionsService {
   constructor(
     @InjectRepository(Transaction)
     private usersRepository: Repository<Transaction>,
+    private dataSource: DataSource, 
   ) {}
 
   save(transaction: Transaction): Promise<Transaction> {
     return this.usersRepository.save(transaction);
+  }
+
+  async createMany(transactions: Transaction[]) {
+    const queryRunner = this.dataSource.createQueryRunner();
+  
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      transactions.forEach(async (transaction) => {
+        await queryRunner.manager.save(transaction);
+      });
+  
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   findAll(): Promise<Transaction[]> {
