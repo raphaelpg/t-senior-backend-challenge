169f7be80c3607c68a84b8e61bc924ebd168988f
raphaelpg <raphael_pinto@outlook.com>
2023-05-04 18:23:46 +0200

    clean auth module

--- practical/src/modules/auth/auth.module.ts
+++ practical/src/modules/auth/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { HeaderApiKeyStrategy } from './auth-header-api-key.strategy';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PassportModule, ConfigModule],
+  imports: [PassportModule],
   providers: [HeaderApiKeyStrategy],
 })
 export class AuthModule {}
