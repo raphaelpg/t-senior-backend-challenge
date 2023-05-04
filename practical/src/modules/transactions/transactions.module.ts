da597bbf1f80516787cb002450c293a135e808ee
raphaelpg <raphael_pinto@outlook.com>
2023-05-04 17:14:20 +0200

    cleaning

--- practical/src/modules/transactions/transactions.module.ts
+++ practical/src/modules/transactions/transactions.module.ts
@@ -3,12 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './transactions.entity';
 import { TransactionsService } from './transactions.services';
 import { TransactionsController } from './transactions.controller';
-import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Transaction]),
-  ],
+  imports: [TypeOrmModule.forFeature([Transaction])],
   providers: [TransactionsService],
   exports: [TransactionsService],
   controllers: [TransactionsController],
