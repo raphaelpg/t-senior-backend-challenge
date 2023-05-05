6a29eb5bbdde246ad8116c5656926cd70f295d8b
raphaelpg <raphael_pinto@outlook.com>
2023-05-05 20:17:16 +0200

    retrieve apiKey on requests

--- practical/src/modules/transactions/transactions.controller.ts
+++ practical/src/modules/transactions/transactions.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Logger, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Headers, Logger, Param, UseGuards } from '@nestjs/common';
 import { TransactionsService } from './transactions.services';
 import { Transaction } from './transactions.entity';
 import { AuthGuard } from '@nestjs/passport';
@@ -9,40 +9,54 @@ export class TransactionsController {
   private readonly logger = new Logger(TransactionsController.name)
 
   @Get()
-  getHello(): string {
-    this.logger.log('Get Transactions');
+  getHello(
+    @Headers('api-key') apiKey: string
+  ): string {
+    this.logger.log('Get Transactions, api-key: ' + apiKey);
     return 'Get Transactions';
   }
 
   // Get last 100 transactions
   @Get('last/:page')
   @UseGuards(AuthGuard('api-key'))
-  getLastTransactions(@Param('page') page: number): Promise<Transaction[]> {
-    this.logger.log('getLastTransactions');
+  getLastTransactions(
+    @Headers('api-key') apiKey: string,
+    @Param('page') page: number
+  ): Promise<Transaction[]> {
+    this.logger.log('getLastTransactions, api-key: ' + apiKey);
     return this.transactionService.findLastWithPagination(page, 100);
   }
 
   // Get last 100 transactions with pagination
   @Get('last/:page/:limit')
-  getLastTransactionsWithPagination(@Param('page') page: number, @Param('limit') limit: number): Promise<Transaction[]> {
-    this.logger.log('getLastTransactionsWithPagination');
+  getLastTransactionsWithPagination(
+    @Headers('api-key') apiKey: string, 
+    @Param('page') page: number, 
+    @Param('limit') limit: number
+  ): Promise<Transaction[]> {
+    this.logger.log('getLastTransactionsWithPaginationn, api-key: ' + apiKey);
     return this.transactionService.findLastWithPagination(page, limit);
   }
 
   // Get all transactions by address
   @Get('address/:address')
   @UseGuards(AuthGuard('api-key'))
-  getAllTransactionsByAddress(@Param('address') address: string): Promise<Transaction[]> {
-    this.logger.log('getAllTransactionsByAddress');
+  getAllTransactionsByAddress(
+    @Headers('api-key') apiKey: string, 
+    @Param('address') address: string
+  ): Promise<Transaction[]> {
+    this.logger.log('getAllTransactionsByAddress, api-key: ' + apiKey);
     return this.transactionService.findAllByAddress(address);
   }
 
   // Get address balance
   @Get('balance/:address')
   @UseGuards(AuthGuard('api-key'))
-  getAddressBalance(@Param('address') address: string): Promise<string> {
-    this.logger.log('getAddressBalance');
+  getAddressBalance(
+    @Headers('api-key') apiKey: string, 
+    @Param('address') address: string
+  ): Promise<string> {
+    this.logger.log('getAddressBalance, api-key: ' + apiKey);
     return this.transactionService.findTotalBalanceByAddress(address);
   }
-  
 }
